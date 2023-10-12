d4f09b6db99d92b7ffd4b66b93df3f7dbdf6e73b
nwyseur <nicolaswyseur@gmail.com>
2023-10-12 15:20:26 +0200

    compose ok

--- srcs/requirements/wordpress/conf/auto_config.sh
+++ srcs/requirements/wordpress/conf/auto_config.sh
@@ -12,4 +12,10 @@ else
                 --dbuser=$SQL_USER \
                 --dbpass=$SQL_PASSWORD \
                 --dbhost=mariadb:3306 --path='/var/www/wordpress'
+
+    wp core install --url=$WP_URL \
+                --title=$WP_TITLE \
+                --admin_user=$WP_ADMIN_USER \
+                --admin_password=$WP_ADMIN_PASSWORD \
+                --admin_email=$WP_ADMIN_EMAIL
 fi
